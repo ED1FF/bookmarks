6cae889d4669f96f7ccd46e304cd26e4f1558157
ed1ff <edifei13@yandex.ua>
2018-05-25 21:01:06 +0300

    make it better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   private
-
+#asdfawe
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
