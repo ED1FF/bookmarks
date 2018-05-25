433be7990d4f8e6efc9ec8cae2a0eaec592cc8e9
ed1ff <edifei13@yandex.ua>
2018-05-25 21:51:09 +0300

    1

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '678354315668445', '58f19e1f292696c6141ad13c72abfdc7',
     scope: 'user_link',
-    # client_options: {
-    #   site: 'https://graph.facebook.com/v3.0',
-    #   authorize_url: "https://www.facebook.com/v3.0/dialog/oauth"
-    # } ,
+    client_options: {
+      site: 'https://graph.facebook.com/v3.0',
+      authorize_url: "https://www.facebook.com/v3.0/dialog/oauth"
+    } ,
     token_params: { parse: :json }
 end
